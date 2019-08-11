c1bd585d29b9ee883e37e6f6d78143100c2085ba
PhiX <PhiXDashGitHub@users.noreply.github.com>
2019-08-11 02:18:01 +0200

    Basic Keyboard Added

---
+++ Assets/KeyBoardSound.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Button))]
+public class KeyBoardSound : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private AudioSource Tone;
+
+    public KeyCode[] KeyBind;
+
+    private Button Key;
+
+    public bool Pressable;
+    public bool FadeOut;
+
+    public EventSystem Events;
+
+    void Start()
+    {
+        Tone = GetComponent<AudioSource>();
+        Key = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        CheckKey();
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (Pressable == true)
+        {
+            Tone.loop = true;
+        }
+
+        Tone.Play();
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        Tone.loop = false;
+
+        if (FadeOut == false)
+        {
+            Tone.Stop();
+        }
+
+        Events.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+    }
+
+    void CheckKey()
+    {
+        for (int i = 0; i < KeyBind.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyBind[i]))
+            {
+                Tone.Play();
+            }
+
+            if (Input.GetKey(KeyBind[i]))
+            {
+                if (Pressable == true)
+                {
+                    Tone.loop = true;
+                }
+
+                Key.Select();
+            }
+            else if (Input.GetKeyUp(KeyBind[i]))
+            {
+                Tone.loop = false;
+
+                if (FadeOut == false)
+                {
+                    Tone.Stop();
+                }
+
+                Events.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+            }
+        }
+    }
+}
