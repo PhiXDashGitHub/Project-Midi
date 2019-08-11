50efc9534cfc77e54e5607505b1057c832123b1d
PhiX <PhiXDashGitHub@users.noreply.github.com>
2019-08-11 16:55:17 +0200

    New Instrument Added

---
+++ Assets/InstrumentPicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InstrumentPicker : MonoBehaviour
+{
+    public GameObject[] Instruments;
+
+    public TextMeshProUGUI Label;
+
+    void Start()
+    {
+        PlayerPrefs.SetString("CurrentInstrument", "Piano");
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Instruments.Length; i++)
+        {
+            if (Instruments[i].name == PlayerPrefs.GetString("CurrentInstrument"))
+            {
+                Instruments[i].SetActive(true);
+            }
+            else
+            {
+                Instruments[i].SetActive(false);
+            }
+        }
+    }
+
+    public void SetInstrument()
+    {
+        PlayerPrefs.SetString("CurrentInstrument", Label.text);
+    }
+}
