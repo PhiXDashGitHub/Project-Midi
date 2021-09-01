cb121bd02d8f4f4176dbc02bbc43999f1e3367d4
PhiX <49991008+PhiXDashGitHub@users.noreply.github.com>
2021-09-01 22:14:05 +0200

    Update ControlKnob.cs

--- Assets/Scripts/ControlKnob.cs
+++ Assets/Scripts/ControlKnob.cs
@@ -6,14 +6,12 @@ using UnityEngine.EventSystems;
 public class ControlKnob : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Range(0, 1)] public float value = 0.25f;
-    float previousValue;
 
     public Vector2 angleLimits;
     public float sensitivity;
 
     bool mouseOver;
     bool dragging;
-    [HideInInspector] public bool valueChanged;
 
     Vector3 dragStart;
     float dragValue;
@@ -43,10 +41,6 @@ public class ControlKnob : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
             value = dragValue + (Input.mousePosition - dragStart).x / sensitivity;
             value = Mathf.Clamp01(value);
         }
-
-        valueChanged = value != previousValue;
-
-        previousValue = value;
     }
 
     public void OnPointerEnter(PointerEventData data)
