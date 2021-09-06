af773decf5fb8e90c3f278cc2b66746c24fc92fd
lepLinus <linus.lepschies@gmail.com>
2021-09-06 21:25:13 +0300

    Fix Intrument Selection

--- Assets/Scripts/UI/InstrumentSelection.cs
+++ Assets/Scripts/UI/InstrumentSelection.cs
@@ -13,23 +13,27 @@ public class InstrumentSelection : MonoBehaviour
 
     Instrument[] arry;
 
-    public void Awake()
-    {
-        arry = Resources.LoadAll<Instrument>("Instruments/");
-    }
-
 
     public void Start()
     {
         MaxamountofInstruments = 6;
         minamountofInstruments = 1;
+        AmountOFInstruments = 0;
+
+        StartCoroutine(LoadAll());
+    }
+
+    public IEnumerator LoadAll()
+    {
+        arry = Resources.LoadAll<Instrument>("Instruments/");
 
         for (int i = 0; i < arry.Length; i++)
         {
             GameObject go = Instantiate(ListObjectPreset, Content.transform);
             go.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, (i * -100) - 50);
-            Content.GetComponent<RectTransform>().sizeDelta = new Vector2(0,i*100);
+            Content.GetComponent<RectTransform>().sizeDelta = new Vector2(0, i * 100);
             go.GetComponent<InstrumentButton>().name = arry[i].name;
         }
+        yield return new WaitForSeconds(0);
     }
 }
