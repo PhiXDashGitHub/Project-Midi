83d7bd312e8887efef2449867c466cf62f6260dc
PhiX <49991008+PhiXDashGitHub@users.noreply.github.com>
2021-10-03 17:38:05 +0200

    Update NoteEditor.cs

--- Assets/Scripts/NoteEditor.cs
+++ Assets/Scripts/NoteEditor.cs
@@ -223,7 +223,7 @@ public class NoteEditor : MonoBehaviour
         {
             Vector2 dir = Input.GetTouch(0).deltaPosition + Input.GetTouch(1).deltaPosition;
 
-            MoveEditorScrollbars(dir / 5);
+            MoveEditorScrollbars(dir / 10);
         }
 
         //Place Notes
